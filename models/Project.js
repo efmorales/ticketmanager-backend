c6e6bc9b1ffaf97d947a5a67f86f3d65d5df3dfe
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-05-04 01:07:17 -0400

    Added organizationRef to Project schema for use with organizations.

--- models/Project.js
+++ models/Project.js
@@ -1,29 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ProjectSchema = new Schema({
+const ProjectSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     members: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
     ],
     issues: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Issue'
-        }
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Issue",
+      },
     ],
 
     // Add any additional fields after here
-}, { timestamps: true });
+    organizationRef: {
+      type: Schema.Types.ObjectId,
+      ref: "Organization",
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Project', ProjectSchema);
